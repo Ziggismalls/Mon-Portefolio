97bac98bbc21d8e4d7c23f23ee6257f2f87a095b
Ziggismalls <frantz.o@icloud.com>
2025-01-28 15:39:14 +0100

    changement de photo plans

---
+++ script.js
@@ -0,0 +1,6 @@
+const map = document.querySelector("#map");
+
+map.addEventListener("click", () => {
+  window.location.href =
+    "https://www.google.fr/maps/place/19+Rue+du+Lion+d'Or,+59140+Dunkerque/@51.0319043,2.3704021,17z/data=!3m1!4b1!4m6!3m5!1s0x47dc8c7d10b5266d:0x203eb40f99b43ed7!8m2!3d51.031901!4d2.372977!16s%2Fg%2F11c2bm8dl0?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D";
+});
